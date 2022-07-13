f8e19f21dca230ab45813b42f85bfa668bc6aed9
Marc Scattolin <marcscattolin@gmail.com>
2022-07-13 12:18:04 -0400

    switched loading bar implementations to one which is easier to work with

--- src/components/Progress.vue
+++ src/components/Progress.vue
@@ -1,4 +1,6 @@
 <script>
+import { gsap } from "gsap";
+
 export default {
   props:{
     percentComplete: Number,
@@ -9,42 +11,130 @@ export default {
         barID: Math.round(Math.random() * 10000000),
     }
   },
-  /*computed: {
-    style() {
-        return 'color: ' + this.color + '; height: 50px; width: 200px;';
+  watch: {
+    percentComplete(newVal, oldVal) {
+      gsap.fromTo('.meterContent',  {width: oldVal + '%'}, {width: newVal + '%', duration: 0.8});
     }
-  },*/
-  mounted: function() {
-    let loadingScript = document.createElement('script');
-    loadingScript.setAttribute('src', './src/components/loadingio/loading-bar.min.js');
-    document.head.appendChild(loadingScript);
-  },
-  updated: function() {
-    document.getElementById(this.barID).ldBar.set(this.percentComplete);
   }
 }
 </script>
 
 <template>
-    <!-- NOTE: updating data-value does not reactively update the loading bar, I do so w/ updated-->
-    <div class="loadContainer">
-        <div v-bind:id="barID" class="ldBar" data-preset="energy"></div>
+    <div v-bind:id="barID" class="meter" ref="theBar">
+    <span class="meterContent" style="width: 0%"></span>
     </div>
 </template>
 
 <style scoped>
-@import './loadingio/loading-bar.min.css';
-.ldBar {
-    /*Using !important is the ONLY WAY this works, for some reason this library directly sets 
-    the width and height in px on the elements*/
-    width: 180px!important;
-    height: 50px!important;
+.meter {
+  width: 90%;
 }
-.ldbar-label {
+/*Written by Chris Coyier, https://freefrontend.com/javascript-progress-bars/*/
+.meter {
+  box-sizing: content-box;
+  height: 20px; /* Can be anything */
+  position: relative;
+  margin: 60px 0 20px 0; /* Just for demo spacing */
+  background: #555;
+  border-radius: 25px;
+  padding: 10px;
+  box-shadow: inset 0 -1px 1px rgba(255, 255, 255, 0.3);
+}
+.meter > span {
+  display: block;
+  height: 100%;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  background-color: rgb(43, 194, 83);
+  background-image: linear-gradient(
+    center bottom,
+    rgb(43, 194, 83) 37%,
+    rgb(84, 240, 84) 69%
+  );
+  box-shadow: inset 0 2px 9px rgba(255, 255, 255, 0.3),
+    inset 0 -2px 6px rgba(0, 0, 0, 0.4);
+  position: relative;
+  overflow: hidden;
+}
+.meter > span:after,
+.animate > span > span {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-image: linear-gradient(
+    -45deg,
+    rgba(255, 255, 255, 0.2) 25%,
+    transparent 25%,
+    transparent 50%,
+    rgba(255, 255, 255, 0.2) 50%,
+    rgba(255, 255, 255, 0.2) 75%,
+    transparent 75%,
+    transparent
+  );
+  z-index: 1;
+  background-size: 50px 50px;
+  animation: move 2s linear infinite;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  overflow: hidden;
+}
+
+.animate > span:after {
+  display: none;
+}
+
+@keyframes move {
+  0% {
+    background-position: 0 0;
+  }
+  100% {
+    background-position: 50px 50px;
+  }
+}
+
+.orange > span {
+  background-image: linear-gradient(#f1a165, #f36d0a);
+}
+
+.red > span {
+  background-image: linear-gradient(#f0a3a3, #f42323);
+}
+
+.nostripes > span > span,
+.nostripes > span::after {
+  background-image: none;
+}
+
+#page-wrap {
+  width: 490px;
+  margin: 80px auto;
+}
+body {
+  background: #333;
   text-align: center;
+  color: #eee;
+  font-family: system-ui, sans-serif;
 }
-.loadContainer {
-    display: flex;
-    justify-content: center;
+h1 {
+  font-size: 42px;
+  font-weight: 600;
+  margin: 0 0 30px;
 }
+pre {
+  background: #000;
+  text-align: left;
+  padding: 20px;
+  margin: 0 auto 30px;
+}
+* {
+  box-sizing: border-box;
+}
+
 </style>
